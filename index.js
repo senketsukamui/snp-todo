3fe04c904713b5febfb70a80de619c14773e4083
senketsukamui <glebpotapov2001@gmail.com>
2020-05-30 20:03:58 +0300

    Reworking whole project's logic

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 const todosAll = document.querySelector(".todo-block__content__todos-all");
-const todosActive = document.querySelector(
-  ".todo-block__content__todos-active"
-);
-const todosCompleted = document.querySelector(
-  ".todo-block__content__todos-completed"
-);
-
 const todoInput = document.querySelector(".todo-input");
-const completeAll = document.querySelector(".todo-complete-all");
-const todosCount = document.querySelector(".count");
-const allSort = document.querySelector(".sorts__all");
-const activeSort = document.querySelector(".sorts__active");
-const completedSort = document.querySelector(".sorts__completed");
-const clearCompletedButton = document.querySelector(".clear");
-window.currentTab = "all";
 
 const createElementWithProps = (elementType, props) => {
   const el = document.createElement(elementType);
@@ -23,151 +9,87 @@ const createElementWithProps = (elementType, props) => {
   });
   return el;
 };
-const changeTodosCount = () => {
-  let todoCompleted = 0;
-  for (node of todosAll.childNodes) {
-    if (!node.className.includes("completed")) {
-      todoCompleted += 1;
-    }
-  }
-  todosCount.innerHTML = `Todos left: ${todoCompleted}`;
+
+const initialState = {
+  todos: [...new Array(10).keys()].map((key) => ({
+    text: new Date().getTime() + key,
+    completed: false,
+    id: Date.now() + key,
+  })),
 };
 
-completeAll.addEventListener("click", (e) => {
-  const allTodos = document.querySelectorAll(".todo");
-  const classnames = Object.values(allTodos).map((e) => e.className);
-  if (classnames.every((cl) => !cl.includes("completed"))) {
-    for (node of allTodos) {
-      node.classList.add("completed");
-      node.childNodes[0].checked = true;
-      changeTodosCount();
-    }
-    return;
-  }
-  if (classnames.every((cl) => cl.includes("completed"))) {
-    for (node of allTodos) {
-      node.classList.remove("completed");
-      node.childNodes[0].checked = false;
-      changeTodosCount();
-    }
-    return;
-  }
+const actions = {
+  create: (text) => {
+    state.todos = [
+      ...state.todos,
+      { text: text, completed: false, id: Date.now() },
+    ];
+  },
+  edit: (newText, id) => {
+    const todoId = state.todos.findIndex((e) => e.id === id);
+    const stateCopy = [...state.todos];
+    stateCopy[todoId].text = newText;
+    state.todos = [...stateCopy];
+  },
+};
 
-  if (classnames.some((cl) => cl.includes("completed"))) {
-    for (node of allTodos) {
-      if (!node.childNodes[0].checked) {
-        node.classList.add("completed");
-        node.childNodes[0].checked = true;
-        changeTodosCount();
-      }
-    }
-    return;
-  }
+const render = (state) => {
+  todosAll.innerHTML = "";
+  todosAll.append(
+    ...state.map((todo) => createToDo(todo.text, todo.completed, todo.id))
+  );
+};
+
+const state = new Proxy(initialState, {
+  set: (target, key, value) => {
+    target[key] = value;
+    render(value);
+    return true;
+  },
 });
 
-const createNewTodo = (todoText) => {
+const createToDo = (text, isCompleted, id) => {
   const todo = createElementWithProps("div", { className: "todo" });
+  todo.setAttribute("id", id);
   const completed = createElementWithProps("input", {
     className: "todo__checkbox",
     type: "checkbox",
+    checked: isCompleted,
   });
 
-  const text = createElementWithProps("div", {
+  const todoText = createElementWithProps("div", {
     className: "todo__text",
-    textContent: todoText,
+    textContent: text,
+  });
+
+  todoText.addEventListener("dblclick", (e) => {
+    todoText.innerHTML = `<input class="todo__text_editable" value="${e.target.innerHTML}" />`;
+    document.querySelector(".todo__text_editable").focus();
+  });
+
+  todoText.addEventListener("keypress", (e) => {
+    console.log();
+    if (event.key == "Enter") {
+      // if (!document.querySelector(".todo__text_editable").value) {
+      // }
+      console.log(event.key);
+      actions.edit(e.target.value, id);
+    }
   });
 
   const deleteButton = createElementWithProps("div", {
     className: "todo__delete-button",
   });
-
-  todo.appendChild(completed);
-  todo.appendChild(text);
-  todo.appendChild(deleteButton);
-  text.addEventListener("dblclick", (e) => {
-    text.innerHTML = `<input class = "todo__text_editable" value="${e.target.innerText}" />`;
-    document.querySelector(".todo__text_editable").focus();
-  });
-  text.addEventListener("keypress", (e) => {
-    if (event.key === "Enter") {
-      if (!document.querySelector(".todo__text_editable").value) {
-        todo.remove();
-        changeTodosCount();
-        return;
-      }
-      text.innerHTML = e.target.value;
-    }
-  });
-  text.addEventListener("focusout", (e) => {
-    if (!document.querySelector(".todo__text_editable").value) {
-      todo.remove();
-      changeTodosCount();
-      return;
-    }
-    text.innerHTML = e.target.value;
-  });
-  deleteButton.addEventListener("click", (e) => {
-    todo.remove();
-    changeTodosCount();
-  });
-  completed.addEventListener("click", (e) => {
-    if (todo.classList[1] == "completed") {
-      todo.classList.remove("completed");
-      if (window.currentTab == "completed") {
-        e.target.parentNode.style.display = "none";
-      }
-      changeTodosCount();
-    } else {
-      todo.classList.add("completed");
-      if (window.currentTab == "active") {
-        e.target.parentNode.style.display = "none";
-      }
-      changeTodosCount();
-    }
-  });
-  todosAll.appendChild(todo);
-  changeTodosCount();
+  todo.append(...[completed, todoText, deleteButton]);
+  return todo;
 };
 
 todoInput.addEventListener("keypress", (event) => {
   const enterKey = "Enter";
-  if (event.key === enterKey && todoInput.value) {
-    createNewTodo(todoInput.value);
+  if (event.key === enterKey) {
+    actions.create(todoInput.value);
     todoInput.value = "";
   }
 });
 
-const displayAll = () => {
-  for (node of todosAll.childNodes) {
-    node.style.display = "flex";
-  }
-};
-
-allSort.addEventListener("click", (e) => {
-  displayAll();
-  window.currentTab = "all";
-});
-
-activeSort.addEventListener("click", (e) => {
-  displayAll();
-  const todoToSort = document.querySelectorAll(".completed");
-  for (node of todoToSort) {
-    node.style.display = "none";
-  }
-  window.currentTab = "active";
-});
-
-completedSort.addEventListener("click", (e) => {
-  displayAll();
-  const todoToSort = document.querySelectorAll(".todo:not(.completed)");
-  for (node of todoToSort) {
-    node.style.display = "none";
-  }
-  window.currentTab = "completed";
-});
-
-clearCompletedButton.addEventListener("click", (e) => {
-  for (node of document.querySelectorAll(".completed")) {
-    node.remove();
-  }
-});
+render(state.todos);
